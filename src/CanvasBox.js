acb707d4066f501b3259487cf2524a2d4d1fc202
Jon Dotsoy <hi@jon.soy>
2019-09-18 01:51:47 -0300

    Update canvasbox

--- src/CanvasBox.js
+++ src/CanvasBox.js
@@ -1,19 +1,32 @@
 const { EOL } = require('os')
 const boxen = require('boxen')
 const chalk = require('chalk')
+const url = require('url')
 const profile = require('../index')
 
 class CanvasBox {
-  draw () {
+  draw() {
+    const profiles = Array.isArray(profile.publishingPrinciples)
+      ? profile.publishingPrinciples.reduce((a, e, i) => {
+        const { hostname } = url.parse(e);
+        if (hostname.endsWith('.com')) a[hostname.replace(/\.com$/, '')] = e;
+        if (hostname.endsWith('.io')) a[hostname.replace(/\.io$/, '')] = e;
+        a[i] = e;
+        a[hostname] = e;
+        return a;
+      }, {})
+      : {};
+
+    // console.log(profiles);
     console.log(
       boxen(
         [
           chalk`{blue.bold ${profile.name}}`,
           `${profile.description}`,
           ``,
-          chalk`{green ${profile.email}} — {green ${profile.website}}`,
-          chalk` — {grey Github:} {green ${profile.profiles.github}}`,
-          chalk` — {grey Twitter:} {green ${profile.profiles.twitter}}`
+          chalk`{green ${profile.email}} — {green ${profile.sameAs}}`,
+          chalk` — {grey Github:} {green ${profiles.github || ''}}`,
+          chalk` — {grey Twitter:} {green ${profiles.twitter || ''}}`
         ].join(EOL),
         {
           padding: 1,
