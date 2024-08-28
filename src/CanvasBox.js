d59d40ac8d18c1edad9598fd789269c0c484b071
Jonathan Delgado <hi@jon.soy>
2024-08-28 12:29:36 -0400

    feat: refactor CanvasBox.js to use @jondotsoy/console-draw and style-text libraries
    The main changes in this commit are the removal of boxen, chalk, os, url dependencies, and replacing them with @jondotsoy/console-draw and style-text.

--- src/CanvasBox.js
+++ src/CanvasBox.js
@@ -1,42 +1,57 @@
-const { EOL } = require('os')
-const boxen = require('boxen')
-const chalk = require('chalk')
-const url = require('url')
+const { URL } = require('url')
+const { c, render } = require('@jondotsoy/console-draw')
+const { styleText } = require('@jondotsoy/style-text')
 const profile = require('../index')
 
 class CanvasBox {
-  draw() {
+  render () {
     const profiles = Array.isArray(profile.publishingPrinciples)
       ? profile.publishingPrinciples.reduce((a, e, i) => {
-        const { hostname } = url.parse(e);
-        if (hostname.endsWith('.com')) a[hostname.replace(/\.com$/, '')] = e;
-        if (hostname.endsWith('.io')) a[hostname.replace(/\.io$/, '')] = e;
-        a[i] = e;
-        a[hostname] = e;
-        return a;
+        const { hostname } = new URL(e)
+        if (hostname.endsWith('.com')) a[hostname.replace(/\.com$/, '')] = e
+        if (hostname.endsWith('.io')) a[hostname.replace(/\.io$/, '')] = e
+        a[i] = e
+        a[hostname] = e
+        return a
       }, {})
-      : {};
+      : {}
 
-    // console.log(profiles);
-    console.log(
-      boxen(
-        [
-          chalk`{blue.bold ${profile.name}}`,
-          `${profile.description.toString().replace(/@\w+/, (e) => chalk`{green.italic ${e}}`)}`,
-          ``,
-          chalk`{green ${profile.email}} — {green ${profile.sameAs}}`,
-          chalk` — {grey Github:} {green ${profiles.github || ''}}`,
-          chalk` — {grey Twitter:} {green ${profiles.twitter || ''}}`
-        ].join(EOL),
-        {
-          padding: 1,
-          borderStyle: 'bold',
-          borderColor: 'blue',
-          align: 'left',
-          float: 'left'
-        }
-      )
+    return render(
+      c('div', { border: { format: 'blue', theme: 'thick', padding: 1 } }, [
+        c('columns', { gap: 0, columns: 2, columnsTemplate: [{ width: 2 }, null] }, [
+          c(''),
+          c('text', `${styleText(['bold', 'blue'], profile.name)}`)
+        ]),
+        c('columns', { gap: 0, columns: 2, columnsTemplate: [{ width: 2 }, null] }, [
+          c(''),
+          c('text', `${profile.description.toString().replace(/@\w+/, (e) => styleText(['green', 'italic'], e))}`)
+        ]),
+
+        c('columns', { gap: 0, columns: 2, columnsTemplate: [{ width: 2 }, null] }, [
+          c('')
+        ]),
+
+        c('columns', { gap: 0, columns: 2, columnsTemplate: [{ width: 2 }, null] }, [
+          c(''),
+          c('text', `${styleText('green', profile.email)} - ${styleText('green', profile.sameAs)}`)
+        ]),
+
+        c('columns', { gap: 0, columns: 2, columnsTemplate: [{ width: 2 }, null] }, [
+          c(''),
+          c('text', ` - ${styleText('grey', 'Github:')} ${styleText('green', profiles.github || '')}`)
+        ]),
+
+        c('columns', { gap: 0, columns: 2, columnsTemplate: [{ width: 2 }, null] }, [
+          c(''),
+          c('text', ` - ${styleText('grey', 'Twitter:')} ${styleText('green', profiles.twitter || '')}`)
+        ])
+      ]),
+      { width: Math.min(process.stdout.columns, 101) }
     )
+  }
+
+  draw () {
+    console.log(this.render())
   }
 }
 
